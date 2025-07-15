86e067c2d9bc626e4351be0e1237af08c64dc103
H-tech473 <78791161+H-tech473@users.noreply.github.com>
2025-07-15 08:54:49 +0530

    Sidebar updated

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,6 @@
 // /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   tutorialSidebar: [
-      'intro',
     {
       type: 'category',
       label: 'Get Started',
